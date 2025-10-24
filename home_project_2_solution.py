0378dc40f821868147ced3e51eadc40972d5ec18
cimerman6 <davido.cim@gmail.com>
2025-10-24 11:38:57 +0200

    Update home_project_2_solution.py

--- home_project_2_solution.py
+++ home_project_2_solution.py
@@ -1,9 +1,36 @@
+#skusime klasicky algoritmus kde jednoducho len prehladame vsetky moznosti a hladame najvacsi ciastocny automorfizmus
+#dufam ze sme mohli pouzit itertools co nam velmi ulahcilo generovanie kombinacii
+
+import itertools
 import networkx as nx
 
-# you are allowed to use any function in networkx library including nx.is_isomorphic, nx.vf2pp_all_isomorphisms, etc.
-
-
 def asymmetric_depth(Graph) -> int:
-    # TODO: implement algorithm that will return the depth of asymmetry
+    n = len(Graph.nodes)
+    vrcholy = list(Graph.nodes)
+    maximum = 0
 
-    return 0
+    for k in range(n, 0, -1):
+        for subset1 in itertools.combinations(vrcholy, k):
+            G1 = Graph.subgraph(subset1)
+
+            for subset2 in itertools.combinations(vrcholy, k):
+                G2 = Graph.subgraph(subset2)
+                rovnake = set(subset1) == set(subset2)
+
+                for x in nx.vf2pp_all_isomorphisms(G1, G2):
+                    if rovnake:
+                        identita = True
+                        for v in x:
+                            if x[v] != v:
+                                identita = False
+                                break
+                        if not identita:
+                            maximum = max(maximum, k)
+                            break
+
+                    else:
+                        maximum = max(maximum, k)
+                        break
+
+    return n - maximum
+
